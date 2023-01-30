e242517fbf6f453f5c6a23abefc2ae1b4f41c505
Alpyne <alpyne.dreams@gmail.com>
2023-01-29 20:25:04 -0800

    Open menu with right click too

--- src/gui/Viewport.h
+++ src/gui/Viewport.h
@@ -37,7 +37,7 @@ namespace chisel
         {
             if (IsMouseOver(viewport))
             {
-                if (Selection.Any() && Mouse.GetButtonDown(Mouse.Middle))
+                if (Selection.Any() && (Mouse.GetButtonUp(Mouse.Right) || Mouse.GetButtonDown(Mouse.Middle)))
                 {
                     ImGui::OpenPopup("Selection");
                 }
