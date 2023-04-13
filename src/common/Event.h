573faa086fc1b425cf44d1f970aaa8087b5959c0
Alpyne <alpyne.dreams@gmail.com>
2023-04-13 02:51:34 -0700

    Allow Event to use std::function
    Need viewport context for render callbacks

--- src/common/Event.h
+++ src/common/Event.h
@@ -1,41 +1,40 @@
 #pragma once
 #include <functional>
-#include <set>
+#include <vector>
 
 namespace chisel
 {
-    // TODO: std::function...
     template <typename... Args>
 	class Event
 	{
 		using F = void(Args...);
 	public:
-		Event& operator +=(F* func) {
-			listeners.insert(func);
+		Event& operator +=(auto func) {
+			listeners.push_back(func);
 			return *this;
 		}
 
-		Event& operator -=(F* func) {
-			listeners.erase(func);
+		Event& operator -=(auto func) {
+			listeners.push_back(func);
 			return *this;
 		}
 
 		void operator ()(Args... args) {
-			for (auto* func : listeners) {
+			for (auto& func : listeners) {
 				func(args...);
 			}
-			for (auto* func : once) {
+			for (auto& func : once) {
 				func(args...);
 			}
 			once.clear();
 		}
 
-		void Once(F* func) {
-			once.insert(func);
+		void Once(auto func) {
+			once.push_back(func);
 		}
 
 	private:
-		std::set<F*> listeners;
-		std::set<F*> once;
+		std::vector<std::function<F>> listeners;
+		std::vector<std::function<F>> once;
 	};
 }
